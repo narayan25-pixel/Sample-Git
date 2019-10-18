302da9a57a05b77b38bfc83824e3f5fcc0245da4
narayan25-pixel <55695796+narayan25-pixel@users.noreply.github.com>
2019-10-18 11:10:23 +0530

    Add files via upload

---
+++ shapes_exercise/shapes_exercise/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shapes_exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shapes shapes = new Shapes(4,2,5,6);
+        }
+    }
+}
---
+++ shapes_exercise/shapes_exercise/Shapes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shapes_exercise
+{
+   
+    class Shapes
+    {
+        int r, l, w, area_rect, area_circle, area_square, a;
+        public Shapes(int r,int l,int w,int a)
+        {
+            area_rect = l * w;
+            area_circle = 22/7*r * r;
+            area_square = a * a;
+            Console.WriteLine("area of rectange is={0}\n area of circle is={1}\n area of square is={2}" ,area_rect,area_circle,area_square);
+            
+        }
+    }
+}
